e504fc0a1aed21072c209f00b53ffae4e9c69937
IsaiasBarbosa <isaiasbarbosa111@gmail.com>
2025-06-29 00:46:36 -0300

    Arrumando o server.js

--- server.js
+++ server.js
@@ -71,11 +71,9 @@ app.delete('/api/menuExecutivo/:id', (req, res) => {
 });
 
 // Servir o frontend buildado (dist) pelo Express
-import path from 'path';
-
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
